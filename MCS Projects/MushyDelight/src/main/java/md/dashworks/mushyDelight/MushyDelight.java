8ec8cfc08ff0375f466aa6cf1e9a20fcd7b5268d
The Dashytified <60381639+Dashytified@users.noreply.github.com>
2026-05-28 04:42:04 +0200

    probe - Moony MC progress
    Nothing too special to see here. Just wanted to format the source file before I move on from this project for now.

--- MCS Projects/MushyDelight/src/main/java/md/dashworks/mushyDelight/MushyDelight.java
+++ MCS Projects/MushyDelight/src/main/java/md/dashworks/mushyDelight/MushyDelight.java
@@ -2,15 +2,19 @@ package md.dashworks.mushyDelight;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class MushyDelight extends JavaPlugin {
+public final class MushyDelight extends JavaPlugin
+{
+    @Override public void onEnable()
+    {
 
-    @Override
-    public void onEnable() {
         // Plugin startup logic
+
     }
 
-    @Override
-    public void onDisable() {
+    @Override public void onDisable()
+    {
+
         // Plugin shutdown logic
+
     }
 }
