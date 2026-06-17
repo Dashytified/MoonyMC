13d58e5f0097ae6340799ae1043b6a40749f0c59
The Dashytified <60381639+Dashytified@users.noreply.github.com>
2026-06-18 00:31:17 +0200

    probe - Moony MC progress
    I have officially kick started a new project named `HaumBaum` which aims at allowing users to use commands `/home` and `/sethome` which you are all familiar with, which if not the case, the command syntax is self explanatory for a familiarized Minecraft player, which is what it will take to use this plugin anyway.

---
+++ MCS Projects/HaumBaum/src/main/java/md/dashworks/haumBaum.java
@@ -0,0 +1,18 @@
+package md.dashworks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class haumBaum extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+        
+    }
+}
